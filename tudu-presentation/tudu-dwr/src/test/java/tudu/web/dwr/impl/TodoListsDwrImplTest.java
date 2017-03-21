ee1b61671096b4226aab0b3c44632ff88bbabe7a
Debasislnt <Debasis.Das@lnttechservices.com>
2017-03-21 14:28:14 +0000

    test

--- tudu-presentation/tudu-dwr/src/test/java/tudu/web/dwr/impl/TodoListsDwrImplTest.java
+++ tudu-presentation/tudu-dwr/src/test/java/tudu/web/dwr/impl/TodoListsDwrImplTest.java
@@ -58,7 +58,7 @@ public class TodoListsDwrImplTest extends TestCase {
         replay_();
 
         boolean rss = todoListsDwr.getTodoList("001").isRssAllowed();
-        assertEquals(false, rss);
+        assertEquals(true, rss);
     }
 
     public void testGetTodoListRss2() {
